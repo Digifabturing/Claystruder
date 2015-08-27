909771db61429185ab3e485f88204da42b87af72
STEFANO PARADISO <stefano.prds@gmail.com>
2015-08-27 18:13:55 +0200

    initial

---
+++ Tool/arduinoClaystruder_0.1/arduinoClaystruder_0.1.ino
@@ -0,0 +1,31 @@
+String readRobot;
+//motor pin mosfet
+int motorPin = 11;
+char c;
+
+void setup() {
+  
+  Serial.begin(115200);
+  Serial1.begin(115200);
+  delay(200);
+  while (!Serial) {
+    ;
+  }
+  Serial.println("ready");
+}
+
+void loop() {
+
+  while (Serial1.available()) {
+    c = Serial1.read();  //gets char from COMAU robot
+    readRobot = String(c); //convert char in string
+    Serial.println(readRobot); //print the value
+    delay(1);  //slow down the looping
+  }
+
+  if (readRobot.length() > 0) {
+    //Serial.println(readRobot);  //debug
+    analogWrite(motorPin, int(c));
+    readRobot = ""; //empty for next input
+  }
+}
